be755269cc576bb698a3cdf19ad0b1b979e89903
Meowway <Sheildonseed@gmail.com>
2025-04-28 16:34:31 +0300

    Correct 2 task

--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -1,21 +1,23 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
     private const string HorizontalAxis = "Horizontal";
+    private const float MinimumTurningSpeed = 0.05f;
 
-    [SerializeField] Transform _ship;
-    [SerializeField] Transform _sail;
+    [SerializeField] Transform _shipDirection;
+    [SerializeField] Transform _sailDirection;
 
     [SerializeField] Transform _windDirection;
 
     [SerializeField] private float _maxSpeed;
+
     [SerializeField] private float _sailRotateSpeed;
     [SerializeField] private float _shipRotateSpeed;
     [SerializeField] private float _maxDegrees;
 
     private Rigidbody _rigidbody;
+
     private float _currentSpeed;
     private float _currentRotationSail;
 
@@ -26,55 +28,38 @@ public class Ship : MonoBehaviour
 
     private void Update()
     {
-        float angleBetweenSailAndWind = CalculateAngleBetween(_sail.forward, _windDirection.forward);
+        CalculateCurrentSpeed();
 
-        CalculateCurrentSpeed(angleBetweenSailAndWind);
-
-        _rigidbody.MovePosition(transform.localPosition + _ship.transform.forward * _currentSpeed * Time.deltaTime);
+        _rigidbody.MovePosition(transform.localPosition + _shipDirection.transform.forward * _currentSpeed * Time.deltaTime);
 
         float rotationInput = Input.GetAxisRaw(HorizontalAxis) * _sailRotateSpeed * Time.deltaTime;
         _currentRotationSail = Mathf.Clamp(_currentRotationSail + rotationInput, -_maxDegrees, _maxDegrees);
-        _sail.transform.localRotation = Quaternion.Euler(0f, _currentRotationSail, 0f);
 
-        if (_currentSpeed > 0.05)
+        _sailDirection.transform.localRotation = Quaternion.Euler(0f, _currentRotationSail, 0f);
+
+        if (_currentSpeed > MinimumTurningSpeed)
         {
             if (Input.GetKey(KeyCode.E))
-                _ship.transform.Rotate(transform.up * _shipRotateSpeed * Time.deltaTime);
+                _shipDirection.transform.Rotate(transform.up * _shipRotateSpeed * Time.deltaTime);
 
             if (Input.GetKey(KeyCode.Q))
-                _ship.transform.Rotate(transform.up * -_shipRotateSpeed * Time.deltaTime);
+                _shipDirection.transform.Rotate(transform.up * -_shipRotateSpeed * Time.deltaTime);
         }
     }
 
-    private float CalculateAngleBetween(Vector3 vectorA, Vector3 vectorB)
+    private void CalculateCurrentSpeed()
+    {
+        _currentSpeed = DotProductBetween(_sailDirection.forward, _shipDirection.forward) * DotProductBetween(_sailDirection.forward, _windDirection.forward) * _maxSpeed;
+    }
+
+    private float DotProductBetween(Vector3 vectorA, Vector3 vectorB)
     {
         float dotProduct = Vector3.Dot(vectorA, vectorB);
 
-        float cos = dotProduct / (vectorA.magnitude * vectorB.magnitude);
-        cos = Mathf.Clamp(cos, -1f, 1f);
+        if (dotProduct < 0)
+            return 0f;
 
-        float angleToTarget = Mathf.Acos(cos) * Mathf.Rad2Deg;
-
-        return Mathf.Abs(angleToTarget);
-    }
-
-    private void CalculateCurrentSpeed(float AngleA)
-    {
-        AngleA = Mathf.Clamp(AngleA, 0, _maxDegrees);
-        float AngleB = CalculateAngleBetween(_ship.forward, _windDirection.forward);
-        float AngleC = CalculateAngleBetween(_ship.forward, _sail.forward);
-
-        if (AngleB > 89 && AngleC > 89)
-        {
-            if (_currentSpeed > 0)
-                _currentSpeed -= 10 * Time.deltaTime;
-
-            return;
-        }
-
-        _currentSpeed = _maxSpeed * (1 - AngleA / _maxDegrees);
-
-        _currentSpeed = Mathf.Clamp(_currentSpeed, 0, _maxSpeed);
+        return dotProduct;
     }
 
     private void OnDrawGizmos()
@@ -83,11 +68,15 @@ public class Ship : MonoBehaviour
             return;
 
         Gizmos.color = Color.red;
-        Ray sailray = new Ray(_sail.transform.position, _sail.transform.forward);
+        Ray sailray = new Ray(_sailDirection.transform.position, _sailDirection.transform.forward);
         Gizmos.DrawRay(sailray.origin, sailray.direction * 100);
 
-        Gizmos.color = Color.blue;
-        Ray shipray = new Ray(_ship.transform.position, _ship.transform.forward);
+        Gizmos.color = Color.green;
+        Ray shipray = new Ray(_shipDirection.transform.position, _shipDirection.transform.forward);
         Gizmos.DrawRay(shipray.origin, shipray.direction * 100);
+
+        Gizmos.color = Color.blue;
+        Ray windray = new Ray(_windDirection.transform.position, _windDirection.transform.forward);
+        Gizmos.DrawRay(windray.origin, windray.direction * 100);
     }
 }
--- Assets/Scripts/Ship/Wind.cs
+++ Assets/Scripts/Ship/Wind.cs
@@ -3,30 +3,45 @@ using UnityEngine.UI;
 
 public class Wind : MonoBehaviour
 {
-    [SerializeField] private Image _image;
-    [SerializeField] private float _timeToChangeDirection = 5f;
+    [SerializeField] private Image _pointerImage;
+    [SerializeField] private float _timeToChangeDirection;
 
     private float _timer;
-    private float _degrees;
+
+    private float _currentRotation;
+    private float _targetRotation;
+
+    private float _rotateSpeed = 125f;
 
     private void Awake()
     {
         _timer = _timeToChangeDirection;
+        _currentRotation = transform.rotation.eulerAngles.y;
     }
 
     private void Update()
     {
         _timer -= Time.deltaTime;
 
+        if (_currentRotation != _targetRotation)
+            if (_currentRotation < _targetRotation)
+            {
+                transform.rotation = Quaternion.Euler(0f, _currentRotation + _rotateSpeed * Time.deltaTime, 0f);
+                _pointerImage.rectTransform.rotation = Quaternion.Euler(0f, 0f, _pointerImage.rectTransform.rotation.eulerAngles.z - _rotateSpeed * Time.deltaTime);
+            }
+
+            if (_currentRotation > _targetRotation) 
+            {
+                transform.rotation = Quaternion.Euler(0f, _currentRotation - _rotateSpeed * Time.deltaTime, 0f);
+                _pointerImage.rectTransform.rotation = Quaternion.Euler(0f, 0f, _pointerImage.rectTransform.rotation.eulerAngles.z + _rotateSpeed * Time.deltaTime);
+            }
+
+        _currentRotation = transform.rotation.eulerAngles.y;
+
         if (_timer > 0)
             return;
 
-        _degrees = Random.Range(0, 2) == 0 ? -25 : 25;
-
-        float newAngle = transform.rotation.eulerAngles.y + _degrees;
-        transform.rotation = Quaternion.Euler(0f, newAngle, 0f);
-        _image.rectTransform.rotation = Quaternion.Euler(0f, 0f, _image.rectTransform.rotation.eulerAngles.z + -_degrees);
-
+        _targetRotation = Random.Range(0, 360);
         _timer = _timeToChangeDirection;
     }
 }
